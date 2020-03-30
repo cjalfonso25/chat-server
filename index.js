e722dd1ec3f83fc6a24bf1c48d868d2ec5224567
Carlo Jude Alfonso <carlojude.alfonso@gmail.com>
2020-03-30 18:26:06 +0800

    Configured for heroku deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 io.on("connection", socket => {
   socket.on("join", ({ username, room }, callback) => {
