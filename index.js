856ace32d700b7979c6d479b6d8d665eda544faa
Carlo Jude Alfonso <carlojude.alfonso@gmail.com>
2020-03-30 17:58:41 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const { addUser, getUser, getUsersInRoom, removeUser } = require("./src/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 5000;
+
+io.on("connection", socket => {
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      username: "Admin",
+      text: `Welcome to room: ${room}!`
+    });
+    socket.broadcast.to(user.room).emit("message", {
+      username: "Admin",
+      text: `${user.username} has joined!`,
+      createdAt: new Date().getTime()
+    });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
+    io.emit("rooms", { room: user.room, users: getUsersInRoom(user.room) });
+
+    if (!user) {
+      return {
+        error
+      };
+    }
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", {
+      username: user.username,
+      text: message,
+      createdAt: new Date().getTime()
+    });
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        username: "Admin",
+        text: `${user.username} has left!`
+      });
+
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
